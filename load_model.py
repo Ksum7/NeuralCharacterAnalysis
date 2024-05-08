9cd17fa5810ea00d49a3555874d3a7e9d9882348
Ksum7 <xamchik02@gmail.com>
2024-05-08 21:25:33 +0300

    add web interface

---
+++ load_model.py
@@ -0,0 +1,132 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import pytorch_lightning as pl
+
+class ModelWithoutMRC(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.loss = torch.nn.BCEWithLogitsLoss()
+        self.GRU = nn.GRU(300, 300, batch_first=True)
+        self.GRU1 = nn.GRU(300, 300, batch_first=True)
+        self.fc1 = nn.Linear(200 * 300, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, 1)
+
+
+    def forward(self, vectors, mrc):
+        x = vectors.unsqueeze(1)
+        x = self.GRU(x.squeeze(1))[0]
+        x = self.GRU1(x)[0]
+        x = x.reshape(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+
+        return x
+
+
+    def training_step(self, batch, batch_idx):
+        inputs, mrc, labels = batch
+        outputs = self(inputs, mrc)
+
+        loss = self.loss(outputs, labels.float())
+
+        self.log('train_loss', loss, on_step=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        inputs, mrc, labels = batch
+        outputs = self(inputs, mrc)
+
+        loss = self.loss(outputs, labels.float())
+
+        self.log('val_loss', loss, on_step=False, on_epoch=True)
+
+
+    def test_step(self, batch, batch_idx):
+        inputs, mrc, labels = batch
+        outputs = F.sigmoid(self(inputs, mrc))
+
+        mse = torch.mean((outputs - labels)**2)
+        self.log('test_mse', mse, on_step=False, on_epoch=True)
+
+    def configure_optimizers(self):
+        return torch.optim.AdamW(self.parameters(), lr=1e-6)
+
+
+class ModelWithMRC(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.loss = torch.nn.BCEWithLogitsLoss()
+        self.GRU = nn.GRU(300, 300, batch_first=True)
+        self.GRU1 = nn.GRU(300, 300, batch_first=True)
+        self.fc1 = nn.Linear(200 * 300, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128 + 27, 64)
+        self.fc4 = nn.Linear(64, 1)
+
+
+    def forward(self, vectors, mrc):
+        x = vectors.unsqueeze(1)
+        x = self.GRU(x.squeeze(1))[0]
+        x = self.GRU1(x)[0]
+        x = x.reshape(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(torch.cat([x, mrc], dim=1)))
+        x = self.fc4(x)
+
+        return x
+
+
+    def training_step(self, batch, batch_idx):
+        inputs, mrc, labels = batch
+        outputs = self(inputs, mrc)
+
+        loss = self.loss(outputs, labels.float())
+
+        self.log('train_loss', loss, on_step=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        inputs, mrc, labels = batch
+        outputs = self(inputs, mrc)
+
+        loss = self.loss(outputs, labels.float())
+
+        self.log('val_loss', loss, on_step=False, on_epoch=True)
+
+
+    def test_step(self, batch, batch_idx):
+        inputs, mrc, labels = batch
+        outputs = F.sigmoid(self(inputs, mrc))
+
+        mse = torch.mean((outputs - labels)**2)
+        self.log('test_mse', mse, on_step=False, on_epoch=True)
+
+    def configure_optimizers(self):
+        return torch.optim.AdamW(self.parameters(), lr=1e-6)
+
+class FinalModel(pl.LightningModule):
+    def __init__(self, models_info):
+        super().__init__()
+        
+        self.loss = torch.nn.BCEWithLogitsLoss()
+        for model_name in ["e", "n", "a", "c", "o"]:
+            model_info = models_info[model_name]
+            if model_info["with_mrc"]:
+                model = ModelWithMRC()
+            else:
+                model = ModelWithoutMRC()
+            model.load_state_dict(model_info["state_dict"])
+            setattr(self, model_name, model)
+
+    def forward(self, v, m):
+        return torch.cat([self.e(v, m), self.n(v, m), self.a(v, m), self.c(v, m), self.o(v, m)], dim=1)
+
+def load_model(path):
+    models_info = torch.load(path)
+    return FinalModel(models_info)
---
+++ processing.py
@@ -0,0 +1,124 @@
+import re
+import numpy as np
+import torch
+import torch.nn.functional as F
+from deep_translator import GoogleTranslator
+from youtube_transcript_api import YouTubeTranscriptApi
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+indices_to_labels = { 0:'e', 1:'n', 2:'a', 3:'c', 4:'o' }
+labels_to_indices = { 'e':0, 'n':1, 'a':2, 'c':3, 'o':4 }
+
+label_names = ['Extroversion', 'Neuroticism', 'Agreeableness', 'Conscientiousness', 'Openness']
+
+def evalText(fModel, nlp, word_data, text):
+    text = GoogleTranslator(source='auto', target='en').translate(text)
+    print(text)
+    fModel.eval()
+    fModel.to(device)
+    with torch.no_grad():
+        input = torch.zeros((200, 300), device=device, dtype=torch.float32)
+        mrc = np.zeros(27)
+        words = []
+        for word in nlp(text)[:200]:
+            if word.lemma_ in word_data:
+                mrc += np.array(word_data[word.lemma_])
+            if word.has_vector and word.is_alpha:
+                words.append(word.vector)
+
+        mrc = torch.tensor(np.array(mrc), device=device, dtype=torch.float32).unsqueeze(0)
+        mrc = mrc / torch.norm(mrc, p=2, dim=1, keepdim=True)
+        mrc = torch.nan_to_num(mrc, nan=0)
+
+        words = torch.tensor(np.array(words), device=device, dtype=torch.float32)
+        input[:words.shape[0]] = words
+        input = input.unsqueeze(0)
+
+        outputs = F.sigmoid(fModel(input, mrc)).squeeze()
+        print(outputs)
+        return outputs
+    
+
+def evalLongText(fModel, nlp, word_data, text):
+    splited_texts = []
+    MAX_WORDS_PER_SPLIT = 200
+
+    sentences = re.split(r'(?<=[.!?])\s+', text)
+
+    current_split = ""
+    current_word_count = 0
+    for sentence in sentences:
+        str_sen =  sentence.strip()
+        if str_sen != "" and any(char.isalnum() for char in sentence):
+            words_in_sentence = len(str_sen.split())
+
+            if current_word_count + words_in_sentence <= MAX_WORDS_PER_SPLIT:
+                current_split += str_sen + " "
+                current_word_count += words_in_sentence
+            elif current_split.strip() != "":
+                splited_texts.append(current_split.strip())
+                current_split = str_sen + " "
+                current_word_count = words_in_sentence
+    if current_split.strip() != "":
+        splited_texts.append(current_split.strip())
+    err_c = 0
+    results = np.zeros(5)
+    for t in splited_texts:
+        try:
+            results += np.array(evalText(fModel, nlp, word_data, t))
+        except Exception as e:
+            print(e)
+            err_c += 1
+    
+    if err_c == len(splited_texts):
+        raise RuntimeError()
+    
+    results /= len(splited_texts) - err_c
+
+    return results.astype(float).tolist()
+
+
+def split_text_into_chunks(text, chunk_size=200):
+    words = text.split()
+    num_chunks = len(words) // chunk_size + (1 if len(words) % chunk_size != 0 else 0)
+    chunks = []
+    for i in range(num_chunks):
+        start_idx = i * chunk_size
+        end_idx = min((i + 1) * chunk_size, len(words))
+        chunk = ' '.join(words[start_idx:end_idx])
+        chunks.append(chunk)
+    return chunks
+
+def extract_youtube_video_id(url):
+    pattern = r'(?:https?://)?(?:www\.)?(?:youtube\.com/watch\?v=|youtu\.be/)([a-zA-Z0-9_-]+)'
+
+    match = re.search(pattern, url)
+    
+    if match:
+        return match.group(1)
+    else:
+        raise ValueError("Invalid YouTube video URL")
+
+def evalYT(fModel, nlp, word_data, url):
+    try:
+        transcript = YouTubeTranscriptApi.get_transcript(extract_youtube_video_id(url))
+    except Exception as e:
+        code = list(YouTubeTranscriptApi.list_transcripts(extract_youtube_video_id(url)))[0].language_code
+        transcript = YouTubeTranscriptApi.get_transcript(extract_youtube_video_id(url), languages=[code])
+
+    yt_texts = split_text_into_chunks(" ".join([item['text'] for item in transcript]))
+    err_c = 0
+    results = np.zeros(5)
+    for t in yt_texts:
+        try:
+            results += np.array(evalText(fModel, nlp, word_data, t))
+        except Exception as e:
+            print(e)
+            err_c += 1
+    
+    if err_c == len(yt_texts):
+        raise RuntimeError()
+    results /= len(yt_texts) - err_c
+    return results.astype(float).tolist()
+
